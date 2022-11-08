fb2bd946fe698fa22921da644778725727227558
ElasticBottle <winstonyeo99@yahoo.com>
2022-11-08 11:53:30 -0800

    Remove unused imports

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,4 @@
-export * from "./lib/CheckoutWithCard";
-export * from "./lib/CheckoutWithEth";
-export * from "./lib/CreateWallet";
-
 //types
-export * from "./interfaces/CheckoutWithCard";
-export * from "./interfaces/CommonCheckoutElementTypes";
-export * from "./interfaces/PaperSDKError";
-export * from "./interfaces/PaperUser";
-export * from "./interfaces/SupportedChainName";
 
 // default customization options and base urls
 export * from "./constants/settings";
