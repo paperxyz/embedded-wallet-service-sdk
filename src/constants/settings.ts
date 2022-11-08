8cf4533c5f8921df71939fcb1eb82e6ef4bf993b
ElasticBottle <winstonyeo99@yahoo.com>
2022-11-08 11:52:26 -0800

    Initial Commit

---
+++ src/constants/settings.ts
@@ -0,0 +1,30 @@
+export const PAPER_APP_URL =
+  process.env.NEXT_PUBLIC_NODE_ENV === "development" ||
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:3000"
+    : process.env.NEXT_PUBLIC_NODE_ENV === "staging" &&
+      typeof window !== "undefined"
+    ? window.location.origin
+    : "https://paper.xyz";
+
+export const PAPER_APP_URL_ALT =
+  process.env.NEXT_PUBLIC_NODE_ENV === "development" ||
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:3000"
+    : process.env.NEXT_PUBLIC_NODE_ENV === "staging" &&
+      typeof window !== "undefined"
+    ? window.location.origin
+    : "https://papercheckout.com";
+
+export const CHECKOUT_WITH_ETH_IFRAME_URL = "/sdk/2022-08-12/checkout-with-eth";
+export const CHECKOUT_WITH_CARD_IFRAME_URL =
+  "/sdk/2022-08-12/checkout-with-card";
+export const CREATE_WALLET_IFRAME_URL = "/sdk/v2/verify-email";
+
+export const DEFAULT_BRAND_OPTIONS = {
+  colorPrimary: "#cf3781",
+  colorBackground: "#ffffff",
+  colorText: "#1a202c",
+  borderRadius: 12,
+  fontFamily: "Open Sans",
+};
