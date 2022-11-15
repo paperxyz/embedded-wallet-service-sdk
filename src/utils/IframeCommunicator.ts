a3695c3d25981a3810b4aca3e6d1cb042d6f4f1c
ElasticBottle <winstonyeo99@yahoo.com>
2022-11-15 14:20:09 -0800

    fix: Handle changing clientId during dev process

--- src/utils/IframeCommunicator.ts
+++ src/utils/IframeCommunicator.ts
@@ -20,47 +20,57 @@ export class IframeCommunicator<T extends { [key: string]: any }> {
     // Creating the IFrame element for communication
     let iframe = document.getElementById(iframeId) as HTMLIFrameElement | null;
 
-    if (!iframe) {
-      iframe = document.createElement("iframe");
+    if (!iframe || iframe.src != link) {
+      if (!iframe) {
+        iframe = document.createElement("iframe");
+        iframe.setAttribute(
+          "style",
+          "width: 0px; height: 0px; visibility: hidden;"
+        );
+        iframe.setAttribute("id", iframeId);
+        document.body.appendChild(iframe);
+      }
       iframe.src = link;
-      iframe.setAttribute(
-        "style",
-        "width: 0px; height: 0px; visibility: hidden;"
+      iframe.onload = IframeCommunicator.onIframeLoadHandler(
+        iframe,
+        this.POST_LOAD_BUFFER_SECONDS
       );
-      iframe.setAttribute("id", iframeId);
-      document.body.appendChild(iframe);
-      const channel = new MessageChannel();
-      iframe.onload = async () => {
-        const promise = new Promise<boolean>(async (res, rej) => {
-          channel.port1.onmessage = (
-            event: MessageEvent<MessageType<void>>
-          ) => {
-            const { data } = event;
-            channel.port1.close();
-            if (!data.success) {
-              return rej(data.error);
-            }
-            isIframeLoaded = true;
-            return res(true);
-          };
-
-          // iFrame takes a bit of time after loading to be ready for message receiving
-          // This is hacky
-          await sleep(this.POST_LOAD_BUFFER_SECONDS);
-          const INIT_IFRAME_EVENT = "initIframe";
-          iframe?.contentWindow?.postMessage(
-            // ? We can probably initialise the iframe with a bunch
-            // of useful information so that we don't have to pass it
-            // through in each of the future call. This would be where we do it.
-            { eventType: INIT_IFRAME_EVENT, data: {} },
-            "*",
-            [channel.port2]
-          );
-        });
-        await promise;
-      };
     }
     this.iframe = iframe;
+  }
+
+  static onIframeLoadHandler(
+    iframe: HTMLIFrameElement,
+    prePostMessageSleepInSeconds: number
+  ) {
+    return async () => {
+      const channel = new MessageChannel();
+      const promise = new Promise<boolean>(async (res, rej) => {
+        channel.port1.onmessage = (event: MessageEvent<MessageType<void>>) => {
+          const { data } = event;
+          channel.port1.close();
+          if (!data.success) {
+            return rej(data.error);
+          }
+          isIframeLoaded = true;
+          return res(true);
+        };
+
+        // iFrame takes a bit of time after loading to be ready for message receiving
+        // This is hacky
+        await sleep(prePostMessageSleepInSeconds);
+        const INIT_IFRAME_EVENT = "initIframe";
+        iframe?.contentWindow?.postMessage(
+          // ? We can probably initialise the iframe with a bunch
+          // of useful information so that we don't have to pass it
+          // through in each of the future call. This would be where we do it.
+          { eventType: INIT_IFRAME_EVENT, data: {} },
+          "*",
+          [channel.port2]
+        );
+      });
+      await promise;
+    };
   }
 
   async call<ReturnData>(procedureName: keyof T, params: T[keyof T]) {
